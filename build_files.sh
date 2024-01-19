23a65c385fd16e3d3ac5025c9b4896130c1911cd
bensonibeabuchi <ibeabuchibenson@gmail.com>
2024-01-19 08:19:26 +0100

    python 3.9

--- build_files.sh
+++ build_files.sh
@@ -1,9 +1,2 @@
-#!/bin/bash
-echo "Python Version:"
-python3 --version
-
-echo "Available Python Versions:"
-ls /usr/bin/python*
-
 pip install -r requirements.txt
-python3.10 manage.py collectstatic
+python3.9 manage.py collectstatic
