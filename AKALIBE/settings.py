711ae4639ee875e868f7d5c718c1e8eddea92eac
bensonibeabuchi <ibeabuchibenson@gmail.com>
2024-01-19 11:54:54 +0100

    static

--- AKALIBE/settings.py
+++ AKALIBE/settings.py
@@ -165,7 +165,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+# STATIC_ROOT = BASE_DIR / "staticfiles"
 
 
 MEDIA_URL = '/media/'
