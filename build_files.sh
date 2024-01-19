e3b8df52f324d2487994b85e07d4053ca9b44404
bensonibeabuchi <ibeabuchibenson@gmail.com>
2024-01-19 07:55:50 +0100

    debugging

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,9 @@
+#!/bin/bash
+echo "Python Version:"
+python3 --version
+
+echo "Available Python Versions:"
+ls /usr/bin/python*
+
 pip install -r requirements.txt
 python3.10 manage.py collectstatic
