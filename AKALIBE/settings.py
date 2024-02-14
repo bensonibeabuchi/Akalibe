ae1ddd6bcd336794baaa5dd4d8e955fbc462aad1
bensonibeabuchi <ibeabuchibenson@gmail.com>
2024-02-14 22:59:34 +0100

    m

--- AKALIBE/settings.py
+++ AKALIBE/settings.py
@@ -23,8 +23,6 @@ DATABASE_URL = 'postgresql://postgres:E*deC1a3fEDcece*cea6-dG6F-4BADDb@monorail.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
